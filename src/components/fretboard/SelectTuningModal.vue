69d60e668b524cad0e197c6ddf937cb1d5cc4d64
Julien BOILEVIN <j.boilevin@lepermislibre.fr>
2022-04-23 12:41:31 +0200

    feat: rework design of tuning selection

--- src/components/fretboard/SelectTuningModal.vue
+++ src/components/fretboard/SelectTuningModal.vue
@@ -13,40 +13,31 @@
         <label
           for="select-tuning-modal"
           class="btn btn-sm btn-circle absolute right-2 top-2"
-          >✕</label
-        >
+        >✕</label>
         <h1 class="text-xl font-bold">Tuning</h1>
 
-        <div class="grid h-20 place-items-center">
-          <div class="flex flex-row-reverse mt-4 gap-2">
+        <div class="grid h-32 place-items-center mt-4">
+          <div class="flex flex-row-reverse gap-8">
             <div
-              class="form-control"
-              v-for="(note, index) in fretboardParametersStore.fretboard
-                .baseNotes"
+              class="flex flex-col gap-2"
+              v-for="(note, index) in fretboardParametersStore.fretboard.baseNotes"
               :key="'note-' + index"
             >
-              <div class="input-group input-group-sm">
-                <button
-                  class="btn btn-square btn-sm btn-primary"
-                  @click="
-                    fretboardParametersStore.changeStringTuning(index, -1)
-                  "
-                >
-                  -
-                </button>
-                <input
-                  type="text"
-                  class="input input-sm input-bordered text-center tuning-note-input w-full"
-                  readonly
-                  :value="note.name"
-                />
-                <button
-                  class="btn btn-square btn-sm btn-primary"
-                  @click="fretboardParametersStore.changeStringTuning(index, 1)"
-                >
-                  +
-                </button>
+              <button
+                class="btn btn-square btn-sm btn-primary"
+                @click="fretboardParametersStore.changeStringTuning(index, -1)"
+              >
+                -
+              </button>
+              <div class="text-center text-lg">
+                {{ note.name }}
               </div>
+              <button
+                class="btn btn-square btn-sm btn-primary"
+                @click="fretboardParametersStore.changeStringTuning(index, 1)"
+              >
+                +
+              </button>
             </div>
           </div>
         </div>
