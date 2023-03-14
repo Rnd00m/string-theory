731c54b7589523d2174cc332843d750dc688cb82
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2023-03-14 21:49:41 +0100

    feat: adapt mobile layout to display fretboard in the center of the app (#6)
    Co-authored-by: Julien BOILEVIN <j.boilevin@lepermislibre.fr>

--- src/views/VisualizerView.vue
+++ src/views/VisualizerView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="fretboard-visualizer">
-    <div class="grid lg:gap-8 gap-4">
+    <div class="absolute right-2 top-2 flex-1 gap-1 justify-end lg:hidden sm:inline-flex">
+      <label class="p-2" for="information-modal">
+        <IconInformation />
+      </label>
+      <label for="app-drawer" class="drawer-button p-2" @click="globalStore.selectedDrawer = 'settings-tuning'">
+        <IconTune />
+      </label>
+      <label for="app-drawer" class="drawer-button p-2" @click="globalStore.selectedDrawer = 'settings'">
+        <IconGear />
+      </label>
+    </div>
+    <div class="grid lg:gap-8">
       <FretboardSettingsLayout></FretboardSettingsLayout>
       <FretboardVisualizer></FretboardVisualizer>
       <InformationComponent></InformationComponent>
@@ -12,13 +23,25 @@
 import FretboardVisualizer from "@/modules/fretboard/components/FretboardVisualizer.vue";
 import FretboardSettingsLayout from "@/components/layouts/FretboardSettingsViewLayout.vue";
 import InformationComponent from "@/modules/information/components/InformationComponent.vue";
+import { useGlobalStore } from "@/stores/globalStore";
+import IconInformation from "@/components/icons/IconInformation.vue";
+import IconGear from "@/components/icons/IconGear.vue";
+import IconTune from "@/components/icons/IconTune.vue";
 
 export default {
   components: {
     FretboardVisualizer,
     FretboardSettingsLayout,
-    InformationComponent
+    InformationComponent,
+    IconInformation,
+    IconGear,
+    IconTune,
   },
+  setup() {
+    const globalStore = useGlobalStore();
+
+    return { globalStore };
+  }
 };
 </script>
 
