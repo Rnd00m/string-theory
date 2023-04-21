3b60b6b50ac90119b610d23c1432b180634010a0
Julien BOILEVIN <j.boilevin@lepermislibre.fr>
2023-04-21 22:57:57 +0200

    feat: recognition exercise

---
+++ src/modules/earTrainingExercise/components/EarTrainingExerciseAnswerButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="btn btn-accent normal-case"
+    :class="buttonClass"
+    @click="checkAnswer"
+  >{{ props.interval.name }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+  interval: Interval;
+  intervalToFind: Interval;
+}
+const props = defineProps<Props>();
+const emit = defineEmits(["earTrainingExercise:answered"]);
+
+const buttonClass= ref<string | null>(null);
+
+function checkAnswer(): void {
+  const isAnswerCorrect: boolean = props.interval.name === props.intervalToFind.name;
+
+  if (!isAnswerCorrect) buttonClass.value = "btn-error";
+  else buttonClass.value = "btn-success";
+
+  emit("earTrainingExercise:answered", isAnswerCorrect);
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/exercises/earTraining/ExerciseIntervalsRecognition.vue
+++ src/views/exercises/earTraining/ExerciseIntervalsRecognition.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="training w-72 sm:w-[32rem]">
+  <div class="training">
     <EarTrainingExercise @exercise-started="startExercise">
       <template #exercise-explication-title>
         Ear training with intervals
@@ -12,10 +12,9 @@
       <template #exercise-detail>
         <div class="stats bg-base-200">
           <div class="stat">
-            <div>Find this interval</div>
-            <div class="stat-value text-xl lg:text-2xl">
+            <div class="stat-value flex items-center justify-center">
               <button
-                class="btn btn-accent btn-md gap-2"
+                class="btn btn-primary btn-md gap-2"
                 @click="playExerciseNotes"
               >
                 <svg-icon type="mdi" :path="mdiPlay" size="28" />
@@ -32,25 +31,27 @@
 
       <template #exercise-responses>
         <div class="grid grid-cols-2 gap-4">
-          <button
-            class="btn btn-primary normal-case"
+          <EarTrainingExerciseAnswerButton
             v-for="interval in intervalsList"
-            :key="interval.name"
-          >{{ interval.name }}</button>
+            :key="interval.name + intervalToFind.name + noteWithIntervalApplied.name"
+            :interval="interval"
+            :interval-to-find="intervalToFind"
+            @earTrainingExercise:answered="consumeAnswerButtonClicked"
+          />
         </div>
       </template>
 
       <template #exercise-modal>
-<!--        <input type="checkbox" id="modal-restart-exercise" class="modal-toggle" :checked="isStartModalDisplayed"/>
+        <input type="checkbox" id="modal-restart-exercise" class="modal-toggle" :checked="isStartModalDisplayed"/>
         <div class="modal">
           <div class="modal-box">
-            <h3 class="font-bold text-lg">Congratulations you've found all the notes</h3>
-            <p class="py-4">You could now restart the exercise with a new note or go back to another exercise.</p>
+            <h3 class="font-bold text-lg">Congratulations you've found the correct interval</h3>
+            <p class="py-4">You could now restart the exercise with a new interval.</p>
             <div class="modal-action">
               <button class="btn" @click="startExercise">Restart</button>
             </div>
           </div>
-        </div>-->
+        </div>
       </template>
     </EarTrainingExercise>
   </div>
@@ -58,6 +59,7 @@
 
 <script setup lang="ts">
 import EarTrainingExercise from "@/modules/earTrainingExercise/components/EarTrainingExercise.vue";
+import EarTrainingExerciseAnswerButton from "@/modules/earTrainingExercise/components/EarTrainingExerciseAnswerButton.vue";
 import { Interval, Note } from "@tonaljs/tonal";
 import { ref } from "vue";
 import { getIntervalsList } from "@/commons/helpers/intervals";
@@ -75,6 +77,7 @@ const rootNote = ref<Note | null>(null);
 const intervalToFind = ref<Interval | null>(null);
 const noteWithIntervalApplied = ref<Note | null>(null);
 const errorsNumber = ref<number>(0);
+const isStartModalDisplayed = ref<boolean>(false);
 
 const sampler = new Tone.Sampler({
   urls: {
@@ -97,6 +100,7 @@ const sampler = new Tone.Sampler({
 }).toDestination();
 
 function startExercise(): void {
+  isStartModalDisplayed.value = false;
   rootNote.value = getRandomNote({
     lowerNote: Note.get("E2"),
     higherNote: Note.get("E5"),
@@ -105,9 +109,7 @@ function startExercise(): void {
   const randomIntervalIndex = getRandomInt(0, intervalsList.value.length);
 
   intervalToFind.value = intervalsList.value[randomIntervalIndex];
-  noteWithIntervalApplied.value = Note.get(Note.transpose(rootNote.value.name, intervalToFind.value.name));
-
-  console.log(rootNote.value.name, intervalToFind.value.name, noteWithIntervalApplied.value.name)
+  noteWithIntervalApplied.value = Note.get(Note.simplify(Note.transpose(rootNote.value.name, intervalToFind.value.name)));
 
   setTimeout(() => {
     playExerciseNotes();
@@ -119,6 +121,11 @@ function playExerciseNotes(): void {
 
   sampler.triggerAttackRelease(rootNote.value.name, 2, now);
   sampler.triggerAttackRelease(noteWithIntervalApplied.value.name, 2, now + 0.5);
+}
+
+function consumeAnswerButtonClicked(isCorrectlyAnswered: boolean): void {
+  if (!isCorrectlyAnswered) errorsNumber.value += 1;
+  else isStartModalDisplayed.value = true;
 }
 </script>
 
