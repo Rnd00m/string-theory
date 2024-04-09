6a89f456b8505d42fa576103cf1637d4d037255d
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2024-04-09 14:49:26 +0200

    feat: only use 512 icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,34 +35,14 @@ export default defineNuxtConfig({
       description: 'A simple app to learn guitar and bass',
       icons: [
         {
-          src: "/icons/icon_144.svg",
-          sizes: "144x144",
-          type: "image/svg+xml",
-        },
-        {
-          src: "/icons/icon_192.svg",
-          sizes: "192x192",
-          type: "image/svg+xml",
-        },
-        {
           src: "/icons/icon_512.svg",
           sizes: "512x512",
           type: "image/svg+xml",
-        },
-        {
-          src: "/icons/icon_1024.svg",
-          sizes: "1024x1024",
-          type: "image/svg+xml",
+          purpose: "any",
         },
         {
           src: "/icons/maskable_icon_512.svg",
           sizes: "512x512",
-          type: "image/svg+xml",
-          purpose: "maskable"
-        },
-        {
-          src: "/icons/maskable_icon_1024.svg",
-          sizes: "1024x1024",
           type: "image/svg+xml",
           purpose: "maskable"
         },
