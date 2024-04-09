acb34261fc4ea603a278b9dde85b80b55a903536
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2024-04-09 13:42:59 +0200

    feat: svg maskable icons rework and new sizes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,16 +35,29 @@ export default defineNuxtConfig({
       description: 'A simple app to learn guitar and bass',
       icons: [
         {
-          src: "/icons/icon.svg",
-          sizes: "512x512",
+          src: "/icons/icon_128.svg",
+          sizes: "128x128",
           type: "image/svg+xml",
-          purpose: "any"
         },
         {
-          src: "/icons/maskable_icon_72.svg",
-          sizes: "72x72",
+          src: "/icons/icon_144.svg",
+          sizes: "144x144",
           type: "image/svg+xml",
-          purpose: "maskable"
+        },
+        {
+          src: "/icons/icon_192.svg",
+          sizes: "192x192",
+          type: "image/svg+xml",
+        },
+        {
+          src: "/icons/icon_512.svg",
+          sizes: "512x512",
+          type: "image/svg+xml",
+        },
+        {
+          src: "/icons/icon_1024.svg",
+          sizes: "1024x1024",
+          type: "image/svg+xml",
         },
         {
           src: "/icons/maskable_icon_128.svg",
@@ -67,6 +80,12 @@ export default defineNuxtConfig({
         {
           src: "/icons/maskable_icon_512.svg",
           sizes: "512x512",
+          type: "image/svg+xml",
+          purpose: "maskable"
+        },
+        {
+          src: "/icons/maskable_icon_1024.svg",
+          sizes: "1024x1024",
           type: "image/svg+xml",
           purpose: "maskable"
         },
