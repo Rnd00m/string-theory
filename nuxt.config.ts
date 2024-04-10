826a6f2aa5185e174baafc0e38a0d2e71c448dc0
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2024-04-10 13:27:40 +0200

    feat: remove icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,23 +40,8 @@ export default defineNuxtConfig({
           type: "image/png",
         },
         {
-          src: "/icons/icon_256.png",
-          sizes: "256x256",
-          type: "image/png",
-        },
-        {
-          src: "/icons/icon_384.png",
-          sizes: "384x384",
-          type: "image/png",
-        },
-        {
           src: "/icons/icon_512.png",
           sizes: "512x512",
-          type: "image/png",
-        },
-        {
-          src: "/icons/icon_1024.png",
-          sizes: "1024x1024",
           type: "image/png",
         },
         {
