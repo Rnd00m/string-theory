bdd970370b2a6e6c48c962ec9597c91975928b77
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2024-05-17 14:19:43 +0200

    save svg tuner

--- pages/tools/tuner.vue
+++ pages/tools/tuner.vue
@@ -18,9 +18,204 @@
 
         <div class="text-sm text-base-content text-right w-20">{{ foundNote.cents }} cents</div>
       </div>
+
+      <svg width="680" height="220" viewBox="0 0 680 220" xmlns="http://www.w3.org/2000/svg">
+        <text
+            x="39.9999"
+            y="50"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          12 000 Hz
+        </text>
+        <g>
+          <path d="M39.9999 92L40.0002 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="39.9999"
+            y="210"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          -40
+        </text>
+        <g>
+          <path d="M54.9998 105L55 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M69.9998 105L70 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M84.9998 105L85 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M99.9998 105L100 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M115 92L115 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M130 105L130 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M145 105L145 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M160 105L160 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M175 105L175 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M190 92L190 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="190"
+            y="210"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          -20
+        </text>
+        <g>
+          <path d="M205 105L205 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M220 105L220 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M235 105L235 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M250 105L250 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M265 92L265 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M280 105L280 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M295 105L295 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M310 105L310 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M325 105L325 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="340"
+            y="50"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          Perfect!
+        </text>
+        <g>
+          <path d="M340 72L340 187" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="340"
+            y="210"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          0
+        </text>
+        <g>
+          <path d="M355 105L355 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M370 105L370 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M385 105L385 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M400 105L400 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M415 92L415 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M430 105L430 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M445 105L445 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M460 105L460 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M475 105L475 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M490 92L490 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="490"
+            y="210"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          20
+        </text>
+        <g>
+          <path d="M505 105L505 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M520 105L520 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M535 105L535 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M550 105L550 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M565 92L565 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M580 105L580 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M595 105L595 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M610 105L610 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <g>
+          <path d="M625 105L625 155" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="640"
+            y="50"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          -40 cents
+        </text>
+        <g>
+          <path d="M640 92L640 167" stroke="#818181" stroke-linecap="round"/>
+        </g>
+        <text
+            x="640"
+            y="210"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          40
+        </text>
+        <g opacity="1">
+          <path d="M40.0049 70L40.0054 190" stroke="#00E676" stroke-linecap="round"/>
+        </g>
+      </svg>
+
+
       <div class="graduations-container flex justify-between items-center relative" ref="graduationsContainer">
         <span
-          class="absolute h-[9.5rem] w-[0.25rem] top-0"
+          class="needle-indicator absolute h-[9.5rem] w-[0.20rem] top-0"
           :class="{
             'bg-error': precision === -2 || precision === 2,
             'bg-warning': precision === -1 || precision === 1,
@@ -33,7 +228,7 @@
           class="graduation-wrapper flex flex-col items-center flex-1"
         >
           <div v-if="isMultipleOf(graduationMarker(i), 20)" class="graduation h-[9rem] w-[0.20rem] bg-base-content"></div>
-          <div v-else-if="isMultipleOf(graduationMarker(i), 10)" class="graduation h-[7rem] w-[0.125rem] bg-base-content"></div>
+          <div v-else-if="isMultipleOf(graduationMarker(i), 10)" class="graduation h-[7rem] w-[0.20rem] bg-base-content"></div>
           <div v-else class="graduation h-[6rem] w-[0.125rem] bg-base-content"></div>
 
           <div
@@ -41,8 +236,9 @@
           >{{ (graduationMarker(i)) % 20 === 0 ? graduationMarker(i) : '&nbsp;' }}</div>
         </div>
       </div>
+
+
       <div class="mt-2 flex justify-center text-base-content font-bold text-8xl">{{ foundNote?.name || '-' }}</div>
-      <div class="needle"></div>
     </div>
 
     <canvas
@@ -70,7 +266,8 @@ const tuner = createTuner({
     console.log('Note:', note)
     foundNote.value = note;
 
-    let position = (note.cents / 40) * (width.value / 2) + (width.value / 2);
+    // let position = (note.cents / 40) * (width.value / 2) + (width.value / 2) + (49 / 2);
+    let position = (0 / 40) * (width.value / 2) + (width.value / 2) + 49 / 2;
     if (position < 0) position = 0;
     if (position > width.value) position = width.value;
     cursorPosition.value = position;
@@ -81,7 +278,7 @@ const tuner = createTuner({
   // (let us know if you find values that work better).
 
   // The number of milliseconds between each pitch detection.
-  updateInterval: 25,
+  updateInterval: 10,
 
   // The frequency of middle A. Defaults to 440Hz.
   a4: 440,
@@ -172,4 +369,7 @@ onUnmounted(() => {
 </script>
 
 <style scoped lang="scss">
+.needle-indicator {
+  transition: all .1s ease-in-out;
+}
 </style>
