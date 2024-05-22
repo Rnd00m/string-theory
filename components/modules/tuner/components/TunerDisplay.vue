318f2464b5acc27e2e77a3634e9fbf61992b0596
Rnd00m <39692856+Rnd00m@users.noreply.github.com>
2024-05-22 22:10:27 +0200

    final version of tuner

--- components/modules/tuner/components/TunerDisplay.vue
+++ components/modules/tuner/components/TunerDisplay.vue
@@ -1,5 +1,11 @@
 <template>
-  <svg width="100%" height="270" viewBox="0 0 1000 270" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    v-if="width >= minDesktopWidth"
+    width="100%"
+    height="270"
+    viewBox="0 0 1000 270"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <g id="tuner">
       <g id="tuner_2">
         <g id="display">
@@ -214,29 +220,213 @@
       </g>
       <g id="bottom indications">
         <text
-            class="graduation-text"
-            xml:space="preserve"
-            style="white-space: pre"
-            dominant-baseline="middle"
-            text-anchor="middle"
+          class="graduation-text"
+          xml:space="preserve"
+          style="white-space: pre"
+          dominant-baseline="middle"
+          text-anchor="middle"
         >
-              <tspan x="45" y="251.864" >-50</tspan>
-            </text>
+            <tspan x="45" y="251.864" >-50</tspan>
+          </text>
         <text
-            class="graduation-text"
-            xml:space="preserve"
-            style="white-space: pre"
-            dominant-baseline="middle"
-            text-anchor="middle"
+          class="graduation-text"
+          xml:space="preserve"
+          style="white-space: pre"
+          dominant-baseline="middle"
+          text-anchor="middle"
         >
-              <tspan x="500.5" y="251.864" >0</tspan>
-            </text>
+            <tspan x="500.5" y="251.864" >0</tspan>
+          </text>
         <text
-            class="graduation-text"
-            dominant-baseline="middle"
-            text-anchor="middle"
+          class="graduation-text"
+          dominant-baseline="middle"
+          text-anchor="middle"
         >
           <tspan x="955.902" y="251.864" >50</tspan>
+        </text>
+      </g>
+    </g>
+  </svg>
+  <svg v-else width="100%" height="320" viewBox="0 0 640 320" xmlns="http://www.w3.org/2000/svg">
+    <g id="mobile tuner" clip-path="url(#clip0_56_2)">
+      <g id="tuner">
+        <g id="display">
+          <g id="side graduation">
+            <path d="M35.0008 253L35.0003 113" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M54.0013 223L54.001 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M73.0016 223L73.0012 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M92.0018 223L92.0015 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M111.002 223L111.002 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M130.003 223L130.002 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M149.003 223L149.003 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="medium graduation">
+            <path d="M168.003 239L168.003 127" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M187.004 223L187.003 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M206.004 223L206.004 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M225.004 223L225.004 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M244.005 223L244.005 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M263.005 223L263.005 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M282.006 223L282.005 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M301.006 223L301.006 143" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="middle graduation">
+            <path d="M320.006 103L320.007 263" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M339.007 143L339.007 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M358.007 143L358.007 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M377.007 143L377.008 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M396.008 143L396.008 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M415.008 143L415.009 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M434.009 143L434.009 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M453.009 143L453.009 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="medium graduation">
+            <path d="M472.009 127L472.01 239" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M491.01 143L491.01 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M510.01 143L510.01 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M529.011 143L529.011 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M548.011 143L548.011 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M567.011 143L567.012 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="graduation">
+            <path d="M586.012 143L586.012 223" stroke="#818181" stroke-linecap="round"/>
+          </g>
+          <g id="side graduation">
+            <path d="M605.012 113L605.013 253" stroke="#818181" stroke-linecap="round"/>
+          </g>
+
+        </g>
+        <g id="indicator">
+          <path
+            d="M35.0137 102L35.0144 264"
+            stroke-width="3"
+            stroke-linecap="round"
+            :transform="cursorPosition"
+            class="needle-indicator"
+            :class="{
+              'fill-error': precision === -2 || precision === 2,
+              'fill-warning': precision === -1 || precision === 1,
+              'fill-success': precision === 0,
+            }"
+          />
+        </g>
+      </g>
+      <g id="top indications">
+        <text
+            v-if="note"
+            class="graduation-text"
+            xml:space="preserve"
+            style="white-space: pre"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          <tspan x="35.0008" y="80" >{{ props.note?.frequency.toFixed(0) }} Hz</tspan>
+        </text>
+        <text
+            class="text-4xl"
+            :class="{
+            'fill-error': precision === -2 || precision === 2,
+            'fill-warning': precision === -1 || precision === 1,
+            'fill-success': precision === 0,
+          }"
+            xml:space="preserve"
+            style="white-space: pre"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          <tspan x="320.006" y="70" v-if="precision === 0">Perfect</tspan>
+          <tspan x="320.006" y="70" v-else-if="precision === 1">High</tspan>
+          <tspan x="320.006" y="70" v-else-if="precision === -1">Low</tspan>
+          <tspan x="320.006" y="70" v-else-if="precision === 2">Too high</tspan>
+          <tspan x="320.006" y="70" v-else-if="precision === -2">Too low</tspan>
+        </text>
+        <text
+            v-if="note"
+            class="graduation-text"
+            xml:space="preserve"
+            style="white-space: pre"
+            dominant-baseline="middle"
+            text-anchor="middle"
+        >
+          <tspan x="605.012" y="80" >{{ props.note?.cents }} cents</tspan>
+        </text>
+
+      </g>
+      <g id="bottom indications">
+        <text
+          class="graduation-text"
+          xml:space="preserve"
+          style="white-space: pre"
+          dominant-baseline="middle"
+          text-anchor="middle"
+        >
+          <tspan x="35.0008" y="290" >-50</tspan>
+        </text>
+        <text
+          class="graduation-text"
+          xml:space="preserve"
+          style="white-space: pre"
+          dominant-baseline="middle"
+          text-anchor="middle"
+        >
+          <tspan x="320.006" y="290" >0</tspan>
+        </text>
+        <text
+          class="graduation-text"
+          dominant-baseline="middle"
+          text-anchor="middle"
+        >
+          <tspan x="605.012" y="290" >50</tspan>
         </text>
       </g>
     </g>
@@ -245,10 +435,19 @@
 
 <script lang="ts" setup>
 import type {Note} from '@chordbook/tuner'
+import {useWindowSize} from "@vueuse/core";
 
 interface Props {
   note: Note | null;
 }
+
+const { width, height } = useWindowSize();
+const minDesktopWidth = ref(1024);
+const desktopTunerDisplayWidth = 910.902; // last right graduation (955.902) - first left graduation (45)
+const mobileTunerDisplayWidth = 570.0112; // last right graduation (605.012) - first left graduation (35.0008)
+const maxPosition = computed(() => {
+  return width.value >= minDesktopWidth.value ? desktopTunerDisplayWidth : mobileTunerDisplayWidth;
+});
 
 const props = withDefaults(defineProps<Props>(), {
   note: null,
@@ -271,7 +470,6 @@ function mapValueToPosition(value: number): number {
   const minValue = -50;
   const maxValue = 50;
   const minPosition = 0;
-  const maxPosition = 910.902;
 
   // Ensure the value is within the expected range
   if (value < minValue) value = minValue;
@@ -280,10 +478,9 @@ function mapValueToPosition(value: number): number {
   // Normalize the value to [0, 1]
   const normalizedValue = (value - minValue) / (maxValue - minValue);
 
-  // Map the normalized value to [0, 910.902]
-  return normalizedValue * (maxPosition - minPosition) + minPosition;
+  // Map the normalized value to [0, maxPossiblePosition]
+  return normalizedValue * (maxPosition.value - minPosition) + minPosition;
 }
-
 
 </script>
 
--- pages/tools/tuner.vue
+++ pages/tools/tuner.vue
@@ -1,113 +1,15 @@
 <template>
   <div id="tuner" class="tuner w-full px-3 md:px-0 md:w-4/5">
-    <div class="tuner-interface">
-      <TunerDisplay
-        :note="foundNote"
-      />
-
-      <div class="mt-2 flex justify-center text-base-content font-bold text-8xl">{{ foundNote?.name || '-' }}{{ foundNote ? ' ' + foundNote.octave : '' }}</div>
-    </div>
-
-    <canvas
-        class="absolute bottom-0 left-0 right-0 w-full h-1/4"
-        ref="visualizer"
-    />
+    <Tuner />
   </div>
 </template>
 
 <script setup lang="ts">
-import type {Note} from '@chordbook/tuner'
-import {createTuner} from '@chordbook/tuner'
-import type {Ref} from "vue";
-import {useElementSize} from "@vueuse/core";
-import TunerDisplay from "~/components/modules/tuner/components/TunerDisplay.vue";
-
 definePageMeta({
   title: 'Tuner'
 })
 
 useSEODescription("Your essential tool for precise tuning on the go. Keep your instrument in perfect pitch with ease.");
-
-const tuner = createTuner({
-  // The callback to call when a note is detected.
-  onNote: note => {
-    foundNote.value = note;
-  },
-
-  // Here are some other settings you can fiddle with and their default values.
-  // (let us know if you find values that work better).
-
-  // The number of milliseconds between each pitch detection.
-  updateInterval: 10,
-
-  // The frequency of middle A. Defaults to 440Hz.
-  a4: 440,
-
-  // The minimum clarity threshold. Anything below this will be ignored
-  clarityThreshold: 0.9,
-
-  // The minimum volume threshold. -1000 means 1/1000th the volume of the loudest sound.
-  minVolumeDecibels: -1000,
-
-  // The minimum and maximum frequencies to detect. To reduce noise, everything else is
-  // filtered out using a lowpass and highpass filter.
-  minFrequency: 27.5, // A0, Lowest note on a piano
-  maxFrequency: 4186.01, // C8, Highest note on a piano
-
-  // The sample rate to use for the audio context.
-  // https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/sampleRate
-  sampleRate: 44100,
-
-  // The size of buffer to use for frequency analysis, which maps to the `fftSize`:
-  // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/fftSize
-  bufferSize: 8192,
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/AnalyserNode/smoothingTimeConstant
-  smoothingTimeConstant: 0.8
-})
-
-const visualizer = ref() as Ref<HTMLCanvasElement>;
-const foundNote = ref(null) as Ref<Note | null>;
-
-const bufferLength = tuner.analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-// Just get the low end of the spectrum
-const displayLength = Math.sqrt(bufferLength) * 2;
-
-function visualize() {
-  const ctx = visualizer.value.getContext("2d")!;
-  visualizer.value.width = visualizer.value.offsetWidth
-  visualizer.value.height = visualizer.value.offsetHeight
-
-  tuner.analyser.getByteFrequencyData(dataArray);
-
-  ctx.clearRect(0, 0, visualizer.value.width, visualizer.value.height);
-
-  const barWidth = (visualizer.value.width / displayLength);
-  let x = 0;
-
-  for (let i = 0; i < displayLength; i++) {
-    const barHeight = (visualizer.value.height * (dataArray[i] / 255));
-
-    ctx.fillStyle = `hsl(221, 83%, 55%)`;
-    ctx.fillRect(x, visualizer.value.height - barHeight, barWidth, barHeight);
-
-    x += barWidth;
-  }
-  requestAnimationFrame(visualize);
-}
-
-onMounted(() => {
-  // Request access to the microphone and begin pitch detection
-  tuner.start();
-
-  // visualize();
-})
-
-onUnmounted(() => {
-  // Stop pitch detection and release the microphone
-  tuner.stop();
-})
 </script>
 
 <style scoped lang="scss">
